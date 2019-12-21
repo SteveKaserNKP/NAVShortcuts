7c71a047ed7e454f9398b1b5627290a43adce280
Steve Kaser <Kisersj@nkparts.com>
2019-12-21 14:27:04 -0500

    creating shortcuts finally

--- createShortcuts.py
+++ createShortcuts.py
@@ -1,8 +1,8 @@
 import os
-import sys
-import glob
 import winshell
 import json
+
+path = os.path.join(os.path.dirname(__file__), "test")
 
 def getSQLName(s):
     if "\\" in s:
@@ -19,16 +19,28 @@ def lnkName(s):
         if s['Profile']:
             rName = rName + f" - {s['Profile']}"
         if s['Configure'] == 'Y':
-            rName = rName + f" - {s['Configure']}"
+            rName = rName + f" - CONFIGURE"
         return rName + '.lnk'
 
-path = os.path.join(os.path.dirname(__file__), "test")
+def deleteShortcuts(path):
+    for f in os.listdir(path):
+        os.remove(os.path.join(path, f))
+
+def createShortcuts(path, systems):
+    for s in systems:
+        lnk_path = os.path.join(path, lnkName(s))
+        # this only creates 2018 shortcuts - needs to determine version
+        lnk_target = os.path.join("C:/","Program Files (x86)","Microsoft Dynamics NAV","110","RoleTailored Client","Microsoft.Dynamics.Nav.Client.exe")
+        winshell.CreateShortcut(Path=lnk_path,
+                                Target=lnk_target,
+                                Arguments='',
+                                StartIn='',
+                                Icon=('',0),
+                                Description=''
+                                )
 
 with open('systems.json') as data:
     systems = json.load(data)
 
-for s in systems:
-    lnk_path = os.path.join(path, lnkName(s))
-    lnk_target = ''
-    # winshell.CreateShortcut(Path=)
-
+deleteShortcuts(path)
+createShortcuts(path, systems)
