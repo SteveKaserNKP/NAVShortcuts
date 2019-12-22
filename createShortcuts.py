43069630ea5fc80529e6b89722bcb11b2346e789
Steve Kaser <Kisersj@nkparts.com>
2019-12-21 22:05:11 -0500

    added ability to create WEB shortcuts

--- createShortcuts.py
+++ createShortcuts.py
@@ -1,9 +1,12 @@
 import os
 import winshell
+from urllib.parse import urljoin
+import webbrowser
 import json
 
 shortcuts_path = os.path.join(os.path.dirname(__file__), "test")
 configs_path = os.path.join(os.path.dirname(__file__), "testConfig")
+icons_path = os.path.join(os.path.dirname(__file__), "icons")
 
 def getSQLName(s):
     if "\\" in s:
@@ -11,48 +14,72 @@ def getSQLName(s):
     else:
         return s
 
-def lnkName(s):
-    rName = f"{s['SystemName']} - {s['NavisionVersion']} - SQL@{getSQLName(s['SQLServer'])}"
+def getLnkPath(s, path):
+    if s['Client'] == 'WEB':
+        rName = f"{s['SystemName']} - WEB - {s['NavisionVersion']} - SQL@{getSQLName(s['SQLServer'])}"
+    else:
+        rName = f"{s['SystemName']} - {s['NavisionVersion']} - SQL@{getSQLName(s['SQLServer'])}"
     if s['NavisionVersion'] == '2009R2':
-        return rName + '.lnk'
+        rName = rName + '.lnk'
     else:
         rName = rName + f" - RTC@{s['RTCServer']} - {s['ClientServicesPort']}"
         if s['Profile']:
             rName = rName + f" - {s['Profile']}"
         if s['Configure'] == 'Y':
             rName = rName + f" - CONFIGURE"
-        return rName + '.lnk'
+        rName = rName + '.lnk'
+    return os.path.join(path, rName)
 
-def getTargetVersion(v):
-    if v == "2009R2":
-        return os.path.join("C:/","Program Files (x86)","Microsoft Dynamics NAV","60","Classic","finsql.exe")
-    elif v == "2016":
-        return os.path.join("C:/","Program Files (x86)","Microsoft Dynamics NAV","90","RoleTailored Client","Microsoft.Dynamics.Nav.Client.exe")
-    elif v == "2017":
-        return os.path.join("C:/","Program Files (x86)","Microsoft Dynamics NAV","100","RoleTailored Client","Microsoft.Dynamics.Nav.Client.exe")
-    elif v == "2018":
-        return os.path.join("C:/","Program Files (x86)","Microsoft Dynamics NAV","110","RoleTailored Client","Microsoft.Dynamics.Nav.Client.exe")
+def getTargetVersion(s):
+    version = s['NavisionVersion']
+    client = s['Client']
+    if version == "2009R2":
+        return os.path.join("C:","Program Files (x86)","Microsoft Dynamics NAV","60","Classic","finsql.exe")
     else:
-        return f'unsupported version passed in to getTargetVersion: {v}'
+        if client == 'WEB':
+            return 'c:\\program files\\internet explorer\\iexplore.exe'
+        else:
+            if version == "2016":
+                return os.path.join("C:/","Program Files (x86)","Microsoft Dynamics NAV","90","RoleTailored Client","Microsoft.Dynamics.Nav.Client.exe")
+            elif version == "2017":
+                return os.path.join("C:/","Program Files (x86)","Microsoft Dynamics NAV","100","RoleTailored Client","Microsoft.Dynamics.Nav.Client.exe")
+            elif version == "2018":
+                return os.path.join("C:/","Program Files (x86)","Microsoft Dynamics NAV","110","RoleTailored Client","Microsoft.Dynamics.Nav.Client.exe")
+            else:
+                return f'unsupported version passed in to getTargetVersion: {version}'
 
-def getArgs(s):
-    v = s['NavisionVersion']
-    if v == "2009R2":
-        args = f"servername={s['SQLServer']}, database={s['DatabaseName']}"
-        if s['Company']:
-            args = args + f", company={s['Company']}"
-        if s['RequireAuthentication']:
-            args = args + f", ntauthentication={'yes' if s['RequireAuthentication'] == 'Y' else 'no'}"
+def getArgs(s, path):
+    sql = s['SQLServer']
+    db = s['DatabaseName']
+    version = s['NavisionVersion']
+    company = s['Company']
+    reqAuth = s['RequireAuthentication']
+    client = s['Client']
+    rtc = s['RTCServer']
+    port = s['ClientServicesPort']
+    instance = s['ServerInstanceName']
+    profile = s['Profile']
+    if version == "2009R2":
+        args = f"servername={sql}, database={db}"
+        if company:
+            args = args + f", company={company}"
+        if reqAuth:
+            args = args + f", ntauthentication={'yes' if reqAuth == 'Y' else 'no'}"
         return args
     else:
-        config = createCUS(s['RTCServer'], s['ClientServicesPort'], s['ServerInstanceName'], configs_path)
-        args = f' -settings:"{os.path.join(configs_path, config)}"'
-        if s['Profile']:
-            profile = s['Profile']
-            args = args + f' -profile:"{profile}"'
-        if s['Configure']:
-            args = args + " -configure"
-        return args
+        if client == 'WEB':
+            if port:
+                return f"http://{rtc}.nkparts.com:{port}/{instance}"
+            else:
+                return f"http://{rtc}.nkparts.com:{port}/{instance}"
+        else:
+            config = createCUS(rtc, port, instance, path)
+            args = f' -settings:"{os.path.join(configs_path, config)}"'
+            if s['Profile']:
+                args = args + f' -profile:"{profile}"'
+            if s['Configure']:
+                args = args + " -configure"
+            return args
 
 def createCUS(rtc, port, instance, path):
     config_name = f"{instance}_{rtc}_{port}.config"
@@ -90,26 +117,39 @@ def createCUS(rtc, port, instance, path):
 
     return config_name
 
+def getIcon(s, icons):
+    version = s['NavisionVersion']
+    client = s['Client']
+    if version == '2009R2':
+        return os.path.join(icons, 'icon_2009R2.ico')
+    else:
+        if client == 'WEB':
+            return os.path.join(icons, 'icon_web.ico')
+        else:
+            return os.path.join(icons, 'icon_windows.ico')
+
 def deleteFilesInDir(path):
     for f in os.listdir(path):
         os.remove(os.path.join(path, f))
 
-def createShortcuts(path, systems):
-    for s in systems:
-        lnk_path = os.path.join(path, lnkName(s))
-        lnk_target = getTargetVersion(s['NavisionVersion'])
-        lnk_args = getArgs(s)
-        winshell.CreateShortcut(Path=lnk_path,
-                                Target=lnk_target,
-                                Arguments=lnk_args,
-                                StartIn='',
-                                Icon=('',0),
-                                Description=''
-                                )
+def createShortcut(path_shortcuts, path_configs, path_icons, system):
+    lnk_path = getLnkPath(system, path_shortcuts)
+    lnk_target = getTargetVersion(system)
+    lnk_args = getArgs(system, path_configs)
+    lnk_icon = getIcon(system, path_icons)
+    winshell.CreateShortcut(
+        Path=lnk_path,
+        Target=lnk_target,
+        Arguments=lnk_args,
+        StartIn='',
+        Icon=(lnk_icon,0),
+        Description=''
+    )
 
 with open('systems.json') as data:
     systems = json.load(data)
 
 deleteFilesInDir(configs_path)
 deleteFilesInDir(shortcuts_path)
-createShortcuts(shortcuts_path, systems)
+for s in systems:
+    createShortcut(shortcuts_path, configs_path, icons_path, s)
