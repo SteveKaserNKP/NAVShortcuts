0e529d525623c393906385bf32625619ab1adc1f
SteveKaserNKP <steve.kaser@nkparts.com>
2019-12-27 17:06:52 -0500

    starting to change over to list of shortcuts rather than iconLabels

--- shortcutsManager.py
+++ shortcutsManager.py
@@ -6,7 +6,7 @@ systems = sc_utils.parseSystemsJSON('systems.json')
 
 window = tk.Tk()
 window.title('Navision Shortcuts Manager')
-window.geometry('1500x960')
+window.geometry('1680x960')
 
 form_frame = tk.Frame(window, bd=1, relief=tk.RIDGE)
 form_frame.widgetName = 'formFrame'
@@ -137,18 +137,75 @@ form_frame.vars = form_vars
 icon_size = 75
 icon_paths = paths.iconPathsSize(icon_size, paths.icons_path_png)
 
-icons_frame = tk.Frame(window, bd=1, relief=tk.RIDGE)
-icons_frame.grid(column=1, row=0, ipadx=5, ipady=5, padx=5, pady=5, sticky='NESW')
+frame_shortcuts = tk.Frame(window, bd=1, relief=tk.RIDGE)
+frame_shortcuts.grid(column=1, row=0, padx=5, pady=5, sticky='EW')
+
+frame_headers = tk.Frame(frame_shortcuts)
+frame_headers.grid(column=1, row=0)
+
+clr_header = 'aquamarine'
+clr_row = 'mint cream'
+
+lbl_sys_name = tk.Label(frame_headers, text='System Name', bd=1, relief=tk.RIDGE, width=20, bg=clr_header)
+lbl_client = tk.Label(frame_headers, text='Client', bd=1, relief=tk.RIDGE, width=15, bg=clr_header)
+lbl_version = tk.Label(frame_headers, text='Navision Version', bd=1, relief=tk.RIDGE, width=15, bg=clr_header)
+lbl_sql_server = tk.Label(frame_headers, text='SQL Server', bd=1, relief=tk.RIDGE, width=15, bg=clr_header)
+lbl_rtc_server = tk.Label(frame_headers, text='RTC Server', bd=1, relief=tk.RIDGE, width=15, bg=clr_header)
+lbl_port = tk.Label(frame_headers, text='Services Port', bd=1, relief=tk.RIDGE, width=15, bg=clr_header)
+lbl_instance = tk.Label(frame_headers, text='Instance Name', bd=1, relief=tk.RIDGE, width=15, bg=clr_header)
+lbl_profile = tk.Label(frame_headers, text='Profile Name', bd=1, relief=tk.RIDGE, width=20, bg=clr_header)
+lbl_configure = tk.Label(frame_headers, text='Configuration', bd=1, relief=tk.RIDGE, width=15, bg=clr_header)
+lbl_db_name = tk.Label(frame_headers, text='Database Name', bd=1, relief=tk.RIDGE, width=15, bg=clr_header)
+lbl_company = tk.Label(frame_headers, text='Company', bd=1, relief=tk.RIDGE, width=15, bg=clr_header)
+lbl_req_auth = tk.Label(frame_headers, text='Require Authentication', bd=1, relief=tk.RIDGE, width=20, bg=clr_header)
+
+lbl_sys_name.grid(column=0, row=0)
+lbl_client.grid(column=1, row=0)
+lbl_version.grid(column=2, row=0)
+lbl_sql_server.grid(column=3, row=0)
+lbl_rtc_server.grid(column=4, row=0)
+lbl_port.grid(column=5, row=0)
+lbl_instance.grid(column=6, row=0)
+lbl_profile.grid(column=7, row=0)
+lbl_configure.grid(column=8, row=0)
+lbl_db_name.grid(column=9, row=0)
+lbl_company.grid(column=10, row=0)
+lbl_req_auth.grid(column=11, row=0)
 
 icons = []
 c = 0
 r = 0
 stack = 10
-for i in range(len(systems)):
-    data = systems[i]
-    c = i % stack
-    if i > 0 and i % stack == 0:
-        r+=1
-    sc_utils.createIconFrame(icons_frame, data, icons, icon_paths, c, r)
+for i, s in enumerate(systems):
+    lbl_sys_name2 = tk.Label(frame_headers, text=s['SystemName'], bd=1, relief=tk.FLAT, width=20, bg=clr_row)
+    lbl_client2 = tk.Label(frame_headers, text=s['Client'], bd=1, relief=tk.FLAT, width=15, bg=clr_row)
+    lbl_version2 = tk.Label(frame_headers, text=s['NavisionVersion'], bd=1, relief=tk.FLAT, width=15, bg=clr_row)
+    lbl_sql_server2 = tk.Label(frame_headers, text=s['SQLServer'], bd=1, relief=tk.FLAT, width=15, bg=clr_row)
+    lbl_rtc_server2 = tk.Label(frame_headers, text=s['RTCServer'], bd=1, relief=tk.FLAT, width=15, bg=clr_row)
+    lbl_port2 = tk.Label(frame_headers, text=s['ClientServicesPort'], bd=1, relief=tk.FLAT, width=15, bg=clr_row)
+    lbl_instance2 = tk.Label(frame_headers, text=s['ServerInstanceName'], bd=1, relief=tk.FLAT, width=15, bg=clr_row)
+    lbl_profile2 = tk.Label(frame_headers, text=s['Profile'], bd=1, relief=tk.FLAT, width=20, bg=clr_row)
+    lbl_configure2 = tk.Label(frame_headers, text=s['Configure'], bd=1, relief=tk.FLAT, width=15, bg=clr_row)
+    lbl_db_name2 = tk.Label(frame_headers, text=s['DatabaseName'], bd=1, relief=tk.FLAT, width=15, bg=clr_row)
+    lbl_company2 = tk.Label(frame_headers, text=s['Company'], bd=1, relief=tk.FLAT, width=15, bg=clr_row)
+    lbl_req_auth2 = tk.Label(frame_headers, text=s['RequireAuthentication'], bd=1, relief=tk.FLAT, width=20, bg=clr_row)
+
+    theRow = i + 1
+    lbl_sys_name2.grid(column=0, row=theRow)
+    lbl_client2.grid(column=1, row=theRow)
+    lbl_version2.grid(column=2, row=theRow)
+    lbl_sql_server2.grid(column=3, row=theRow)
+    lbl_rtc_server2.grid(column=4, row=theRow)
+    lbl_port2.grid(column=5, row=theRow)
+    lbl_instance2.grid(column=6, row=theRow)
+    lbl_profile2.grid(column=7, row=theRow)
+    lbl_configure2.grid(column=8, row=theRow)
+    lbl_db_name2.grid(column=9, row=theRow)
+    lbl_company2.grid(column=10, row=theRow)
+    lbl_req_auth2.grid(column=11, row=theRow)
+    # c = i % stack
+    # if i > 0 and i % stack == 0:
+    #     r+=1
+    # sc_utils.createIconFrame(frame_shortcuts, system, icons, icon_paths, c, r)
 
 window.mainloop()
